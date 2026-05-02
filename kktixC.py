e1d9ba4c9032643c8fdefb15712534bac48804f3
sherry1349 <sherry4858@gmail.com>
2026-05-03 02:17:58 +0800

    Add files via upload

--- kktixC.py
+++ kktixC.py
@@ -1,5 +1,8 @@
+from flask import Flask
 import requests
 import os
+
+app = Flask(__name__)
 
 DISCORD_WEBHOOK = os.getenv("DISCORD_WEBHOOK")
 
@@ -9,20 +12,19 @@ def send_discord(msg):
     requests.post(DISCORD_WEBHOOK, json={"content": msg})
 
 def check_ticket():
-    headers = {
-        "User-Agent": "Mozilla/5.0"
-    }
-
+    headers = {"User-Agent": "Mozilla/5.0"}
     res = requests.get(URL, headers=headers)
     text = res.text
-
     return ("自行選位" in text or "電腦配位" in text)
 
-def main():
+@app.route("/")
+def run_check():
     if check_ticket():
         send_discord("🔥 KKTIX 有票了！")
+        return "有票了"
     else:
-        print("還沒票")
+        return "還沒票"
 
 if __name__ == "__main__":
-    main()
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
