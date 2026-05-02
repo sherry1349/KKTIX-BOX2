8e78e01360d2068969ff7dd6035ce79964ad4196
sherry1349 <sherry4858@gmail.com>
2026-05-03 02:11:10 +0800

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,5 @@ WORKDIR /app
 COPY . .
 
 RUN pip install -r requirements.txt
-RUN playwright install --with-deps
 
 CMD ["python", "kktixC.py"]
--- kktixC.py
+++ kktixC.py
@@ -1,28 +1,26 @@
-from playwright.sync_api import sync_playwright
 import requests
 import os
 
 DISCORD_WEBHOOK = os.getenv("DISCORD_WEBHOOK")
 
+URL = "https://kktix.com/events/akiba1/registrations/new"
+
 def send_discord(msg):
     requests.post(DISCORD_WEBHOOK, json={"content": msg})
 
 def check_ticket():
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
-        page = browser.new_page()
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
 
-        page.goto("https://kktix.com/events/akiba1/registrations/new")
-        page.wait_for_timeout(5000)
+    res = requests.get(URL, headers=headers)
+    text = res.text
 
-        content = page.content()
-        browser.close()
-
-        return ("自行選位" in content or "電腦配位" in content)
+    return ("自行選位" in text or "電腦配位" in text)
 
 def main():
     if check_ticket():
-        send_discord("🔥 KKTIX 釋票了！快搶！")
+        send_discord("🔥 KKTIX 有票了！")
     else:
         print("還沒票")
 
